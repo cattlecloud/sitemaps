b7f43c80467d2e82ffc3e2ec4614163c44a89305
Seth Hoenig <shoenig@duck.com>
2025-11-01 07:05:59 -0500

    encoding: ensure the modified date is in the correct format

--- calendars_test.go
+++ calendars_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/shoenig/test/must"
 )
 
-func Test_DayFrom(t *testing.T) {
+func TestCalendarDay_DayFrom(t *testing.T) {
 	t.Parallel()
 
 	now := time.Date(2025, 11, 1, 6, 15, 0, 0, time.UTC)
@@ -18,4 +18,17 @@ func Test_DayFrom(t *testing.T) {
 	must.Eq(t, 2025, cd.Year)
 	must.Eq(t, 11, cd.Month)
 	must.Eq(t, 1, cd.Day)
+}
+
+func TestCalendarDay_ISO(t *testing.T) {
+	t.Parallel()
+
+	cd := CalendarDay{
+		Year:  2025,
+		Month: 11,
+		Day:   1,
+	}
+
+	iso := cd.ISO()
+	must.Eq(t, "2025-11-01", iso)
 }
